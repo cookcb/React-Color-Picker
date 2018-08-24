cfc72e0a295a7b72e13bad317468169b3a737a62
Christian Cook <cbcook321@gmail.com>
2018-08-24 14:54:42 -0400

    Removed SliderBox

--- src/ColorPicker.js
+++ src/ColorPicker.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import SliderBox from "./SliderBox";
 import ColorBox from "./ColorBox";
+import ColorSlider from "./ColorSlider";
 
 const styles = {
   background: "#ffb84d",
@@ -9,10 +9,20 @@ const styles = {
 };
 
 class ColorPicker extends Component {
+  retrieveColorValue = (color, value) => {};
+
   render() {
     return (
       <div style={styles}>
-        <SliderBox />
+        <div>
+          <ColorSlider
+            color={"red"}
+            hex={"#ff4d4d"}
+            getValue={this.retrieveColorValue.bind(this)}
+          />
+          <ColorSlider color={"green"} hex={"#33cc33"} />
+          <ColorSlider color={"blue"} hex={"#0066ff"} />
+        </div>
         <ColorBox red={"0"} green={"0"} blue={"0"} />
       </div>
     );
